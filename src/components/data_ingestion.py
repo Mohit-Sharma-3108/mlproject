9a55d43db2adee430d754d39ae771416574ff14a
Mohit Sharma <mohit1d.lp@gmail.com>
2025-01-25 13:52:31 +0530

    Added data transformation files

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -8,6 +8,8 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
+from src.components.data_transformation import DataTransformationConfig
+from src.components.data_transformation import DataTransformation
 
 @dataclass
 class DataIngestionConfig:
@@ -24,12 +26,17 @@ class DataIngestion:
 
         try:
             df = pd.read_csv(r"C:\Users\Mohit\Desktop\Mohit\Projects\mlproject\notebook\data\stud.csv")
-            logging.info("Read dataset as a dataframe")
+            logging.info("Read dataset as a dataframe..")
 
             os.makedirs(op.dirname(self.ingestion_config.train_data_path), exist_ok=True)
+            
+            logging.info("Normalizing column names..")
+            df.rename({"race/ethnicity": "race_ethnicity"}, axis=1, inplace=True)
+            df.columns = [ col.replace(" ", "_") if " " in col else col for col in df.columns]
+
             df.to_csv(self.ingestion_config.raw_data_path, index=False, header=True)
 
-            logging.info("Train test split initiated")
+            logging.info("Train test split initiated..")
             train_set, test_set = train_test_split(df, test_size=0.2, random_state=42)
 
             train_set.to_csv(self.ingestion_config.train_data_path, index=False, header=True)
@@ -47,4 +54,9 @@ class DataIngestion:
         
 if __name__ == "__main__":
     obj = DataIngestion()
-    obj.initiate_data_ingestion()
+    train_path, test_path = obj.initiate_data_ingestion()
+
+    data_transformation = DataTransformation()
+    data_transformation.initiate_data_transformation(train_path=train_path, test_path=test_path)
+
+
